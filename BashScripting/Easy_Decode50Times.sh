a8661fe1fcf39a04b93d1bef5da65e3146f9fe69
Kelvin.tc.Law <73468336+KelvinLaw-jpg@users.noreply.github.com>
2024-12-25 09:11:03 +0000

    Update Easy_Decode50Times.sh

--- BashScripting/Easy_Decode50Times.sh
+++ BashScripting/Easy_Decode50Times.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#TASK
+# 1.read input from the file
+# 2.use function to decode the file
+# 3.do process in a loop
+
 #do whatever is in the loop for 50 times
 for i in {1..50}; do 
         #save the printed and decoded argument number 1 to the var CONTENT
